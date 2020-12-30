b875ea57785a804664b193aaa530aa40f3377e8c
Pedro Henrique <pedroh1510@hotmail.com>
2020-12-30 19:31:39 -0300

    feat: add helmet

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from 'express'
 import routes from './routes'
 import { errors } from 'celebrate'
 import cors from 'cors'
+import helmet from 'helmet'
 
 const app = express()
 
+app.use(helmet())
 app.use(cors())
 app.use(express.json())
 app.use(routes)
