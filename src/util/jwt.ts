903b237360c85d9f8d87ce75e5a566cb707a3278
Pedro Henrique <pedroh1510@hotmail.com>
2020-12-30 11:27:20 -0300

    feat: add secret para jwt expirar

--- src/util/jwt.ts
+++ src/util/jwt.ts
@@ -1,5 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 export const generateToken = (data: string): string => {
-  return jwt.sign({ [data]: data }, process.env.SECRET)
+  return jwt.sign({ [data]: data }, process.env.SECRET, {
+    expiresIn: process.env.SECRET_TIME
+  })
 }
