9ced5dd212f22688bd840b276fc3756bd205f251
Pedro Henrique <pedroh1510@hotmail.com>
2020-12-30 10:10:54 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import routes from './routes'
 import { errors } from 'celebrate'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use(errors())
