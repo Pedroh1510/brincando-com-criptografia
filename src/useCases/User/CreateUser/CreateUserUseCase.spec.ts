9093fb518c4a6085592f5920bd5fc81cf4c8f1ac
Pedroh1510 <pedroh1510@hotmail.com>
2021-01-14 12:27:07 -0300

    test: add e refatoração de testes CreateUserUseCase

--- src/useCases/User/CreateUser/CreateUserUseCase.spec.ts
+++ src/useCases/User/CreateUser/CreateUserUseCase.spec.ts
@@ -1,25 +1,49 @@
-import { UserError } from '@util/errors'
+import { MissingParamError, UserError } from '@util/errors'
 import { Users } from '@entities/Users'
-import { ICreateUserDTO } from './CreateUserDTO'
+import { ICreateUserDTORequest } from './CreateUserDTO'
 import { CreateUserUseCase } from './CreateUserUseCase'
 import faker from 'faker'
 import { UserRepositorySpy } from '../mocks/UserRepositorySpy'
+import { badRequest, forbidden, noContent } from '@util/httpErrors'
+import { makeFakeUserData } from '@util/makeFaker'
 
-const makeRequest: ICreateUserDTO = {
-  userEmail: faker.internet.email(),
-  userDocument: faker.internet.userAgent(),
-  userName: faker.name.firstName(),
-  userPassword: faker.internet.password()
+interface makeRequestDTO {
+  userDocument?: string
+  userEmail?: string
+  userName?: string
+  userPassword?: string
 }
+
+const makeRequest = ({
+  userDocument,
+  userEmail,
+  userName,
+  userPassword
+}: makeRequestDTO): ICreateUserDTORequest => {
+  return {
+    body: {
+      userDocument:
+        userDocument === undefined ? makeData.document : userDocument,
+      userEmail: userEmail === undefined ? makeData.email : userEmail,
+      userName: userName === undefined ? makeData.name : userName,
+      userPassword:
+        userPassword === undefined ? makeData.password : userPassword
+    }
+  }
+}
+
+const makeData = makeFakeUserData()
+
 const makeUser = (): Users => {
   const user = new Users({
-    document: makeRequest.userDocument,
-    email: makeRequest.userEmail,
-    name: makeRequest.userName,
-    password: makeRequest.userPassword
+    document: faker.internet.userAgent(),
+    email: faker.internet.email(),
+    name: faker.name.firstName(),
+    password: faker.internet.password()
   })
   return user
 }
+
 const makeSut = () => {
   const userRepositorySpy = new UserRepositorySpy()
   const sut = new CreateUserUseCase(userRepositorySpy)
@@ -29,14 +53,47 @@ const makeSut = () => {
 describe('Test Create User UseCase', () => {
   test('Cadastra um usuário', async () => {
     const { sut } = makeSut()
-    const response = await sut.execute(makeRequest)
-    expect(response).toBeUndefined()
+    const response = await sut.execute(makeRequest({}))
+    expect(response).toEqual(noContent())
   })
-  test('Retorna um erro usuário ja existe', () => {
+
+  test('Retorna um erro usuário ja existe', async () => {
     const { sut, userRepositorySpy } = makeSut()
     userRepositorySpy.user = makeUser()
-    const promise = sut.execute(makeRequest)
-    // eslint-disable-next-line jest/valid-expect
-    expect(promise).rejects.toThrow(new UserError('User already exists'))
+    const response = await sut.execute(makeRequest({}))
+
+    expect(response).toEqual(forbidden(new UserError('User already exists')))
+  })
+
+  test('Retorna um erro MissingParamError userDocument', async () => {
+    const { sut, userRepositorySpy } = makeSut()
+    userRepositorySpy.user = makeUser()
+    const response = await sut.execute(makeRequest({ userDocument: '' }))
+
+    expect(response).toEqual(badRequest(new MissingParamError('userDocument')))
+  })
+
+  test('Retorna um erro MissingParamError userName', async () => {
+    const { sut, userRepositorySpy } = makeSut()
+    userRepositorySpy.user = makeUser()
+    const response = await sut.execute(makeRequest({ userName: '' }))
+
+    expect(response).toEqual(badRequest(new MissingParamError('userName')))
+  })
+
+  test('Retorna um erro MissingParamError userEmail', async () => {
+    const { sut, userRepositorySpy } = makeSut()
+    userRepositorySpy.user = makeUser()
+    const response = await sut.execute(makeRequest({ userEmail: '' }))
+
+    expect(response).toEqual(badRequest(new MissingParamError('userEmail')))
+  })
+
+  test('Retorna um erro MissingParamError userPassword', async () => {
+    const { sut, userRepositorySpy } = makeSut()
+    userRepositorySpy.user = makeUser()
+    const response = await sut.execute(makeRequest({ userPassword: '' }))
+
+    expect(response).toEqual(badRequest(new MissingParamError('userPassword')))
   })
 })
