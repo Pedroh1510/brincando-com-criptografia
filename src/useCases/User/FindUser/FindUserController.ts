b8fbe2c0c2522e3bf23841a924041ef5c0e0101e
Pedroh1510 <pedroh1510@hotmail.com>
2021-01-14 23:31:58 -0300

    fix: corrigido tipo de parametro

--- src/useCases/User/FindUser/FindUserController.ts
+++ src/useCases/User/FindUser/FindUserController.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from 'express'
 import { IControllerDTO } from '@util/IControllerDTO'
 import { FindUserUseCase } from './FindUserUseCase'
+import { IFindUserRequestDTO } from './FindUserDTO'
 
 export class FindUserController implements IControllerDTO {
   constructor(private findUserUseCase: FindUserUseCase) {}
   async handle(request: Request, response: Response): Promise<Response> {
-    const { body, statusCode } = await this.findUserUseCase.execute(request)
+    const { body, statusCode } = await this.findUserUseCase.execute(
+      (request as unknown) as IFindUserRequestDTO
+    )
 
     return response.status(statusCode).send(body)
   }
