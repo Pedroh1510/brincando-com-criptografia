ed324b718f32d7ee2e5cada4747fb88081bcaba5
Pedroh1510 <pedroh1510@hotmail.com>
2021-01-30 01:14:17 -0300

    feat: add body-parser

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,15 @@ import { errors } from 'celebrate'
 import cors from 'cors'
 import helmet from 'helmet'
 import compression from 'compression'
+import bodyParser from 'body-parser'
 
 const app = express()
 
 app.use(compression())
 app.use(helmet())
 app.use(cors())
-app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(routes)
 app.use(errors())
 
